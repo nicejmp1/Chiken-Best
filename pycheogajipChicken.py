4887b910886228f369b12a61ee10d409da84b47c
박정민 <163364733+nicejmp1@users.noreply.github.com>
2024-05-08 16:47:25 +0900

    Update pycheogajipChicken.py

--- pycheogajipChicken.py
+++ pycheogajipChicken.py
@@ -18,7 +18,8 @@ urls = [
 
 # 현재 날짜 가져오기
 current_date = datetime.now().strftime("%Y-%m-%d")
-filename = f"cheogajipChicken_{current_date}.json"
+folder_path = "cheogajipChicken"
+filename = f"{folder_path}/cheogajipChicken_{current_date}.json"
 base_url = "https://www.cheogajip.co.kr"
 
 # 웹드라이버 설정
